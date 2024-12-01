9dd1a138cd8d298a8a38fe13ac6f3d7e43af1cbd
AMEY SUNIL Kulkarni <axk1195@cs.bham.ac.uk>
2024-12-01 20:20:40 +0530

    Project Files

---
+++ Assistant_code/src/main.py
@@ -0,0 +1,160 @@
+
+#Google Assistant funtions
+
+from __future__ import print_function
+import argparse
+import json
+import os.path
+import pathlib2 as pathlib
+import os
+import subprocess
+import google.oauth2.credentials
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+from google.assistant.library.file_helpers import existing_file
+from google.assistant.library.device_helpers import register_device
+
+import faulthandler
+faulthandler.enable()
+
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+
+WARNING_NOT_REGISTERED = """
+    This device is not registered. This means you will not be able to use
+    Device Actions or see your device in Assistant Settings. In order to
+    register this device follow instructions at:
+
+    https://developers.google.com/assistant/sdk/guides/library/python/embed/register-device
+"""
+
+ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
+USER_PATH = os.path.realpath(os.path.join(__file__, '..', '..','..'))
+
+def process_event(event):
+    """Pretty prints events.
+
+    Prints all events that occur with two spaces between each new
+    conversation and a single space between turns of a conversation.
+
+    Args:
+        event(event.Event): The current event to process.
+    """
+    print(event)
+    if event.type == EventType.ON_MUTED_CHANGED:
+        print("Mic mute is set to: " + str(event.args["is_muted"]))
+
+    if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        subprocess.Popen(["aplay", "{}/sample-audio-files/Fb.wav".format(ROOT_PATH)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print()
+
+    if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
+            event.args and not event.args['with_follow_on_turn']):
+        print()
+
+    if (event.type == EventType.ON_RESPONDING_STARTED and event.args and not event.args['is_error_response']):
+        print(event.args)
+
+    if event.type == EventType.ON_RESPONDING_FINISHED:
+        print(event.args)
+
+    if event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED:
+        usrcmd=event.args["text"]
+
+    if event.type == EventType.ON_DEVICE_ACTION:
+        for command, params in event.actions:
+            print('Do command', command, 'with params', str(params))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--device-model-id', '--device_model_id', type=str,
+                        metavar='DEVICE_MODEL_ID', required=False,
+                        help='the device model ID registered with Google')
+    parser.add_argument('--project-id', '--project_id', type=str,
+                        metavar='PROJECT_ID', required=False,
+                        help='the project ID used to register this device')
+    parser.add_argument('--nickname', type=str,
+                        metavar='NICKNAME', required=False,
+                        help='the nickname used to register this device')
+    parser.add_argument('--device-config', type=str,
+                        metavar='DEVICE_CONFIG_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'googlesamples-assistant',
+                            'device_config_library.json'
+                        ),
+                        help='path to store and read device configuration')
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='path to store and read OAuth2 credentials')
+    parser.add_argument('--query', type=str,
+                        metavar='QUERY',
+                        help='query to send as soon as the Assistant starts')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s ' + Assistant.__version_str__())
+
+    args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+
+    device_model_id = None
+    last_device_id = None
+    try:
+        with open(args.device_config) as f:
+            device_config = json.load(f)
+            device_model_id = device_config['model_id']
+            last_device_id = device_config.get('last_device_id', None)
+    except FileNotFoundError:
+        pass
+
+    if not args.device_model_id and not device_model_id:
+        raise Exception('Missing --device-model-id option')
+
+    # Re-register if "device_model_id" is given by the user and it differs
+    # from what we previously registered with.
+    should_register = (
+        args.device_model_id and args.device_model_id != device_model_id)
+
+    device_model_id = args.device_model_id or device_model_id
+
+    with Assistant(credentials, device_model_id) as assistant:
+        events = assistant.start()
+        subprocess.Popen(["aplay", "{}/sample-audio-files/Startup-Female.wav".format(ROOT_PATH)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        device_id = assistant.device_id
+        print('device_model_id:', device_model_id)
+        print('device_id:', device_id + '\n')
+
+        # Re-register if "device_id" is different from the last "device_id":
+        if should_register or (device_id != last_device_id):
+            if args.project_id:
+                register_device(args.project_id, credentials,
+                                device_model_id, device_id, args.nickname)
+                pathlib.Path(os.path.dirname(args.device_config)).mkdir(
+                    exist_ok=True)
+                with open(args.device_config, 'w') as f:
+                    json.dump({
+                        'last_device_id': device_id,
+                        'model_id': device_model_id,
+                    }, f)
+            else:
+                print(WARNING_NOT_REGISTERED)
+
+        for event in events:
+            if event.type == EventType.ON_START_FINISHED and args.query:
+                assistant.send_text_query(args.query)
+            process_event(event)
+
+
+if __name__ == '__main__':
+    main()
